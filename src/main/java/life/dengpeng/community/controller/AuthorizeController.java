4dc3f764a94bb31709c045e5b4470072b909c76c
dp54321 <1178182855@qq.com>
2019-05-21 15:21:31 +0800

    登录功能部分ä¸完善

--- src/main/java/life/dengpeng/community/controller/AuthorizeController.java
+++ src/main/java/life/dengpeng/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author dp
  * @create 2019-05-21 10:45
@@ -27,8 +29,9 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                            @RequestParam(name = "state") String state
-                            ){
+                            @RequestParam(name = "state") String state,
+                           HttpServletRequest httpServletRequest
+                            ) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -37,8 +40,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getGithubUser(accessToken);
-        System.out.println(githubUser.getName());
-        return "index";
+        if (githubUser != null) {
+            //登录成功 用户信息保存到session中
+            httpServletRequest.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
     }
-
 }
